67f6c77a1066f66812e72d89e14fc799365e1e94
정승호 <fz062981@naver.com>
2018-04-14 13:15:43 +0900

    material design accept

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
 
-import { AppComponent } from './app.component';
-import { IndexComponent } from './index/index.component';
-import { HomeComponent } from './home/home.component';
-import { JqueryComponent } from './jquery/jquery.component';
-import { AngularComponent } from './angular/angular.component';
+import {AppComponent} from './app.component';
+import {IndexComponent} from './index/index.component';
+import {HomeComponent} from './home/home.component';
+import {JqueryComponent} from './jquery/jquery.component';
+import {AngularComponent} from './angular/angular.component';
+import {RouterModule, Routes} from '@angular/router';
+import {MatButtonModule, MatCardModule, MatIconModule, MatMenuModule, MatToolbarModule} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FlexLayoutModule} from '@angular/flex-layout';
+
+const route: Routes = [
+  {path: '', component: IndexComponent, children: [
+    {path: 'home', component: HomeComponent},
+    {path: 'jquery', component: JqueryComponent},
+    {path: 'angular', component: AngularComponent}
+  ]}
+];
 
 
 
@@ -19,9 +31,19 @@ import { AngularComponent } from './angular/angular.component';
     AngularComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(route),
+    MatToolbarModule,
+    MatMenuModule,
+    MatIconModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    FlexLayoutModule,
+    MatCardModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
