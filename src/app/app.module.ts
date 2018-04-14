b7f59214e769b8b01dcdbbd067e88c66cf29168e
정승호 <fz062981@naver.com>
2018-04-14 13:14:22 +0900

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
+import { HomeComponent } from './home/home.component';
+import { JqueryComponent } from './jquery/jquery.component';
+import { AngularComponent } from './angular/angular.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    IndexComponent,
+    HomeComponent,
+    JqueryComponent,
+    AngularComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
